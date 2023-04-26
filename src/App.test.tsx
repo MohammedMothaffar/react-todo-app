0ddb604fd15f6d5cf7aca5d162afe779b457d325
Mohammed Mothaffar <109213119+MohammedMothaffar@users.noreply.github.com>
2023-04-26 21:55:58 +0300

    new config with ci-cd

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 
 import { render, screen } from '@testing-library/react';
-import {App} from './App';
+import { App } from './App';
 describe('tests the app root component', function () {
     it('should render the awesome text ', function () {
         render(<App />);
