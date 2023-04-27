f062ddbee0076fd4ac12ed37715c18b537ad6a41
Mohammed Mothaffar <109213119+MohammedMothaffar@users.noreply.github.com>
2023-04-27 14:34:04 +0300

    ww

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import  React from 'react';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 describe('tests the app root component', function () {
     it('should render the awesome text ', function () {
-        render(<App />)
-        expect(screen.getByText(/awesome/)).toBeInTheDocument()
+        render(<App />);
+        expect(screen.getByText(/awesome/)).toBeInTheDocument();
     });
 });
