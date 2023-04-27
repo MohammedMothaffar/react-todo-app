43f74a90292ae1926a31aa705df0567d41c8be6d
Mohammed Mothaffar <109213119+MohammedMothaffar@users.noreply.github.com>
2023-04-27 14:32:12 +0300

    qq

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import '@testing-library/jest-dom';
-
+import  React from 'react';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 describe('tests the app root component', function () {
     it('should render the awesome text ', function () {
-        render(<App />);
-        expect(screen.getByText(/awesome/)).ToBeInTheDocument();
+        render(<App />)
+        expect(screen.getByText(/awesome/)).toBeInTheDocument()
     });
 });
